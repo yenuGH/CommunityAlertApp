a91bf41d0de2bb83cbabeec5a60fb3aa499046d5
yenu <35312312+yenuGH@users.noreply.github.com>
2023-11-18 14:03:18 -0800

    change some things

--- app/src/main/java/com/CommunityAlert/communityalert/activities/LoginPageActivity.java
+++ app/src/main/java/com/CommunityAlert/communityalert/activities/LoginPageActivity.java
@@ -3,14 +3,27 @@ package com.CommunityAlert.communityalert.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.communityalert.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginPageActivity extends AppCompatActivity {
+
+    private EditText editTextUsernameField;
+    private EditText editTextPasswordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        initializeFields();
     }
+
+    private void initializeFields() {
+        editTextUsernameField = findViewById(R.id.loginPageUsernameField);
+        editTextPasswordField = findViewById(R.id.loginPageUsernameField);
+    }
+
 }
